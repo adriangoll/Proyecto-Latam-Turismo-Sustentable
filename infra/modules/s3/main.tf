56140ce18e369a1505de7539701750093d1f8969
MarianaGil68 <marianagil@gmail.com>
2026-04-13 00:19:55 -0300

    Agrega bucket datalake y estructura base en S3

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -10,18 +10,6 @@ resource "aws_s3_bucket_versioning" "datalake" {
   }
 }
 
-resource "aws_s3_object" "raw_owid_co2_prefix" {
-  bucket  = aws_s3_bucket.datalake.id
-  key     = "raw/owid_co2/"
-  content = ""
-}
-
-resource "aws_s3_object" "bronze_co2_emissions_prefix" {
-  bucket  = aws_s3_bucket.datalake.id
-  key     = "bronze/co2_emissions/"
-  content = ""
-}
-
 resource "aws_s3_object" "raw_prefix" {
   bucket  = aws_s3_bucket.datalake.id
   key     = "raw/"
@@ -43,5 +31,40 @@ resource "aws_s3_object" "silver_prefix" {
 resource "aws_s3_object" "gold_prefix" {
   bucket  = aws_s3_bucket.datalake.id
   key     = "gold/"
+  content = ""
+}
+
+resource "aws_s3_object" "raw_owid_co2_prefix" {
+  bucket  = aws_s3_bucket.datalake.id
+  key     = "raw/owid_co2/"
+  content = ""
+}
+
+resource "aws_s3_object" "bronze_co2_emissions_prefix" {
+  bucket  = aws_s3_bucket.datalake.id
+  key     = "bronze/co2_emissions/"
+  content = ""
+}
+resource "aws_s3_object" "raw_worldbank_tourism_prefix" {
+  bucket  = aws_s3_bucket.datalake.id
+  key     = "raw/worldbank_tourism/"
+  content = ""
+}
+
+resource "aws_s3_object" "raw_owid_transport_prefix" {
+  bucket  = aws_s3_bucket.datalake.id
+  key     = "raw/owid_transport/"
+  content = ""
+}
+
+resource "aws_s3_object" "bronze_tourism_arrivals_prefix" {
+  bucket  = aws_s3_bucket.datalake.id
+  key     = "bronze/tourism_arrivals/"
+  content = ""
+}
+
+resource "aws_s3_object" "bronze_transport_mode_prefix" {
+  bucket  = aws_s3_bucket.datalake.id
+  key     = "bronze/transport_mode/"
   content = ""
 }
