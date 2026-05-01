67f59c4df84eb4629134701b54af7202aec2acb2
MarianaGil68 <marianagil@gmail.com>
2026-05-01 00:23:09 -0300

    fix: agregar wrapper run_transformation con argumentos por defecto

--- pipelines/transformation/__init__.py
+++ pipelines/transformation/__init__.py
@@ -1,1 +1,4 @@
-from pipelines.transformation.run_transformation import run_all as run_transformation
+from pipelines.transformation.run_transformation import run_all as _run_all
+
+def run_transformation():
+    _run_all(dry_run=False, sources=[], local_bronze={})
