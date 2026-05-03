fc775788c8f811ebb778bd595966f276553ffd38
adriangoll <maselchuecoleon@gmail.com>
2026-05-02 21:21:59 -0300

    fix: modificar agregar tasks

--- dags/dag_datalake_core_monthly.py
+++ dags/dag_datalake_core_monthly.py
@@ -44,6 +44,12 @@ with DAG(
         python_callable=wrapper_procesamiento,
         op_kwargs={"script_func": run_ingestion},
     )
+    # Validar bronze
+    task_validate_bronze = PythonOperator(
+        task_id="task_validate_bronze",
+        python_callable=run_validation,
+        op_kwargs={"layer": "bronze", "source": "co2_emissions"},
+    )
 
     # Tarea Capa Silver (Bronze → Silver)
     task_silver = PythonOperator(
@@ -96,4 +102,4 @@ with DAG(
     )
 
     # Dependencias: orden correcto
-    task_ingest_bronze >> task_silver >> task_validate_silver >> task_crawlers_silver >> task_gold >> task_validate_gold >> task_crawlers_gold
+    task_ingest_bronze >> task_validate_bronze >> task_silver >> task_validate_silver >> task_crawlers_silver >> task_gold >> task_validate_gold >> task_crawlers_gold
